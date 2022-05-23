91633f40efe70f6f3985cbea20cf4c6fff8de1af
Ahmad <engr.ahmadmustafeen@gmail.com>
2022-05-23 15:25:22 +0500

    everything back to place

---
+++ src/components/MarkerTipComponent.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+
+const MarkerTipIcon = props => {
+    const {className} = props
+    return(
+        <div style={{ position: "relative" }}>
+        <div className={className}>
+            <div className="item-hints"><div className="hint" data-position="2">
+                <span className="hint-radius" />
+                <span className="hint-dot"><img src={require("../assets/images/pin.png")} width="60px" /></span>
+                <div className="hint-content do--split-children">
+                    <p><img src={require("../assets/images/pop.jpeg")} /></p>
+                </div>
+            </div>
+            </div>
+        </div>
+    </div>
+    )
+}
+
+export {MarkerTipIcon}
