e484f0285e931b559dc3ba43439755dba547c4f7
Ahmed Kareem <ahmed.kareem.elshawadfy@gmail.com>
2022-05-28 09:15:00 +0200

    add marsad jazan

--- src/containers/marsad_jazan/index.js
+++ src/containers/marsad_jazan/index.js
@@ -7,15 +7,16 @@ import "./style.css"
 
 const LandingPage = () => {
     return (
+
         <div className="weather">
             <span className="cloud-1"></span>
             <span className="cloud-2"></span>
             <span className="cloud-3"></span>
             <span className="cloud-4"></span>
             <span className="sun"></span>
-           <MarkerTipIcon className="tip1"/>
-           <MarkerTipIcon className="tip2"/>
-           <MarkerTipIcon className="tip3"/>
+            <MarkerTipIcon className="tip1" />
+            <MarkerTipIcon className="tip2" />
+            <MarkerTipIcon className="tip3" />
         </div>
     )
 }
